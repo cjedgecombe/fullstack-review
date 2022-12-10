138a1332de40ca5dc0246ad0142b857c21d7cbcc
CJ Edgecombe <cjledgecombe@gmail.com>
2022-12-09 21:23:46 -0700

    Complete displaying top 25 repos

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const RepoList = (props) => (
-  <div>
+const RepoList = (props) => {
+
+  var sldkfj;
+
+  return <div>
     <h4> Repo List Component </h4>
-    There are {props.repos.length} repos.
+    These are the top {props.repos.length} most forked repos in the database.
+
+    <ol>
+      {props.repos.map((repo) => {
+        return <li key={repo.githubID}>{repo.name} by {repo.owner.name} with {repo.forks} forks</li>
+      })}
+    </ol>
   </div>
-)
+}
 
 export default RepoList;
